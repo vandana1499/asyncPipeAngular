e43da843795b1fe021db6d03e418664426832023
Vandana Gupta <Vandanagupta1499@gmail.com>
2021-04-06 12:00:50 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'async-pipe';
+  promise:Promise<string>
+  constructor()
+  {
+    this.promise=this.getPromise();
+  }
+  getPromise():Promise<string>
+  {
+    return new Promise((resolve,reject)=>{
+      setTimeout(()=>resolve("Process is complete"),3000)
+    })
+  }
+  /* promiseData:string;
+  constructor()
+  {
+    this.getPromise().then((v:string)=>{
+      this.promiseData=v;
+    })
+  }
+  getPromise()
+  {
+    return new Promise((resolve,reject)=>{
+      setTimeout(()=>resolve("Process Compeleted"),3000)
+    })
+  } */
 }
